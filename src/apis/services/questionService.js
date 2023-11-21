3637f936b4db5e15954f4829a2e58afa86eebbfa
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-21 14:59:02 +0530

    Create questionService.js

---
+++ src/apis/services/questionService.js
@@ -0,0 +1,35 @@
+const {getQuestionsByParameters} = require('../services/fetchQuestion')
+const generateQuestions = async(questionType, exam, subject, chapter, topic, subTopic, numOfQuestions) => {
+  // Logic to generate the specified number of questions based on the parameters
+  const filteredQuestions = await getQuestionsByParameters(questionType, exam, subject, chapter, topic, subTopic);
+//  return  filteredQuestions
+  
+  const selectedQuestions = await selectRandomQuestions(filteredQuestions, numOfQuestions);
+  const questionsWithOptions = selectedQuestions.map(question => {
+    return {
+      question: question.question,
+      options: question.options
+    };
+  });
+  return questionsWithOptions;
+};
+const selectRandomQuestions = (questions, numOfQuestions) => {
+  const selectedQuestions = [];
+  const totalQuestions = questions.length;
+  if (numOfQuestions >= totalQuestions) {
+    return questions; // Return all questions if the requested number is greater than or equal to the total available questions
+  }
+  // Randomly select numOfQuestions from the available questions
+  while (selectedQuestions.length < numOfQuestions) {
+    const randomIndex = Math.floor(Math.random() * totalQuestions);
+    const randomQuestion = questions[randomIndex];
+    if (!selectedQuestions.includes(randomQuestion)) {
+      selectedQuestions.push(randomQuestion);
+    }
+  }
+  console.log('++++++++++++++', selectedQuestions);
+  return selectedQuestions;
+};
+module.exports = {
+  generateQuestions,
+};
